e2c564e4dba13bed10829374d76f9e3807e90f1e
Богдан <brazbogdan@gmail.com>
2025-04-17 15:30:14 +0300

    updated commit

---
+++ controllers/reviewController.js
@@ -0,0 +1,26 @@
+const reviewService = require("../services/reviewService");
+
+exports.addComment = async (req, res) => {
+  try {
+    const comment = await reviewService.createComment(
+      req.params.gameId,
+      req.user._id,
+      req.body.text
+    );
+
+    req.io.to(req.params.gameId).emit("newComment", comment); // WebSocket
+
+    res.status(201).json(comment);
+  } catch (err) {
+    res.status(500).json({ message: "Не вдалося додати коментар" });
+  }
+};
+
+exports.getCommentsByGame = async (req, res) => {
+  try {
+    const comments = await reviewServiceService.getCommentsByGameId(req.params.gameId);
+    res.status(200).json(comments);
+  } catch (err) {
+    res.status(500).json({ message: "Не вдалося завантажити коментарі" });
+  }
+};
